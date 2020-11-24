706a5eb28c90aca31133bb62db3af391c69bd403
jsierra93 <jsierra93@hotmail.com>
2020-11-24 10:07:36 -0500

    Se modifica gitignore y se crea nuevo proyecto para conexion via jms

---
+++ webflux-rabbitmq-jms/src/main/java/co/com/jsierra/jmsrabbitmq/JmsrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package co.com.jsierra.jmsrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsrabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsrabbitmqApplication.class, args);
+	}
+
+}
---
+++ webflux-rabbitmq-jms/src/test/java/co/com/jsierra/jmsrabbitmq/JmsrabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jsierra.jmsrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsrabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
